3059daf55c9e2e81fb8dbb821a497c984c374bf5
Jacob Ekedahl <jacob.ekedahl@if.no>
2019-12-13 21:13:04 +0100

    Save all videos under temp/videos

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import sys as s
 
+import utils.file_handler as file_handler
 from downloader import svtplaywrapper
 
 if __name__ == "__main__":
+    file_handler.create_folderstructure_if_not_exists()
     if (len(s.argv) -1 < 1):
-        print("need more arguments! (--dlv --file nameOfTxtFile nameOfFolder numberOfEpisodes)")
+        print("need more arguments! (--dlv --file nameOfTxtFile numberOfEpisodes)")
         exit()
 
     if (s.argv[1] == "--dlv"):
@@ -15,13 +17,11 @@ if __name__ == "__main__":
 
         if (s.argv[2] == "--file"):
             name_textfile = s.argv[3]
-            name_folder =  s.argv[4]
             number_of_episodes = s.argv[5]
-            svtplaywrapper.start(name_textfile, name_folder, number_of_episodes)
+            svtplaywrapper.start(name_textfile, number_of_episodes)
         elif (s.argv[2] == "--url"):
             url_path = s.argv[3]
-            name_folder = s.argv[4]
-            svtplaywrapper.download_video(url_path, name_folder)
+            svtplaywrapper.download_video(url_path)
 
         print("finnished downloading!")
         exit()
--- src/utils/file_handler.py
+++ src/utils/file_handler.py
@@ -1,8 +1,17 @@
 import os
 
+TEMPFOLDERNAME = "temp"
+VIDEOFOLDERNAME = "temp/videos"
+
+def create_folderstructure_if_not_exists():
+    if not os.path.exists(VIDEOFOLDERNAME):
+        os.makedirs(VIDEOFOLDERNAME)
+
+def get_full_path_videos():
+    return os.path.join(str(os.getcwd()), VIDEOFOLDERNAME)
 
 def get_full_path_temp():
-    return os.path.join(str(os.getcwd()), "temp")
+    return os.path.join(str(os.getcwd()), TEMPFOLDERNAME)
 
 def get_full_path_folder(folder_name):
     return os.path.join(get_full_path_temp(), folder_name)
@@ -10,6 +19,3 @@ def get_full_path_folder(folder_name):
 def get_all_urls_from_file(file_name):
     text_file_path = os.path.join(get_full_path_temp(), file_name)
     return [line.rstrip('\n') for line in open(text_file_path)]
-
-def get_all_full_path_mkv(folder_name):
-    return "hi"
