229c608e324c671bc2666bd6db96624ffa4d9aa7
Tiago Redaelli <tiago.redaelli@gmail.com>
2019-12-27 15:16:55 +0100

    single db interface

---
+++ src/db/video_repo.py
@@ -0,0 +1,139 @@
+""" 
+    video_repo.py is a database interface to all videos managed by the program.
+
+    API: 
+
+    *   insert(video)
+    *   find_all_not_dl(): 
+    *   find_all_dl()
+    *   find_by_url(url)
+    *   find_by_file(videoFileName)
+    *   find_by_show(targetShow)
+    *   find_by_show_not_dl(targetShow)
+    *   find_by_show_and_season(targetShow, season)
+    *   find_by_show_and_season_not_dl(targetShow, season)
+    *   save_after_download(url, mp4_fullpath)
+    *   set_downloaded_flag(url, flag)
+    *   change_fullpath(file, fullpath)
+    *   set_scenes(videoFileName, scenes):
+    *   get_shows()
+    *   get_show_seasons(targetShow):
+    *   delete_by_file(videoFileName):
+    *   delete_by_url(url):
+    *   delete_show(targetShow)
+"""
+
+import pymongo
+import json
+import os 
+
+from db.video import Video 
+from pathlib import Path
+
+
+secret = None
+with open(".secret.json") as json_file:
+    secret = json.load(json_file)
+    
+dbName = secret['dbname']
+url = secret['url']
+mongoClient = pymongo.MongoClient(url)
+db = mongoClient[dbName]
+videoCollection = db["videos"]
+
+
+# API
+
+def insert(srcs_video):
+    if not isinstance(srcs_video, Video):
+        raise TypeError('Inappropriate type: {}, Expected: {}.'.format(type(srcs_video), type(Video)))
+    if find_by_url(srcs_video.url) is not None: 
+        return None
+    x = videoCollection.insert_one(srcs_video.__dict__)
+    return x.inserted_id
+
+def find_all_not_dl():
+    return list(videoCollection.find({"downloaded": False}))
+
+def find_all_dl():
+     return list(videoCollection.find({"downloaded": True}))
+
+def find_by_url(url):
+    return videoCollection.find_one({"url": url})
+
+def find_by_file(filename):
+    return videoCollection.find_one({"file": filename})
+
+def find_by_show(targetShow):
+    return list(videoCollection.find({"show": targetShow}))
+
+def find_by_show_not_dl(targetShow):
+    return list( videoCollection.find({
+        "$and": [
+            {"show": targetShow}, 
+            {"downloaded": False}
+        ]
+    }))
+
+def find_by_show_and_season(targetShow, season):
+    return list( videoCollection.find({
+        "$and": [
+            {"show": targetShow}, 
+            {"season": season}
+        ]
+    }))
+    
+def find_by_show_and_season_not_dl(targetShow, season):
+    return list( videoCollection.find({
+        '$and': [
+            {'show': targetShow}, 
+            {"season": season},
+            {"downloaded": False}
+        ]
+    }))
+
+def save_after_download(url, mp4_fullpath):
+    return videoCollection.update_one({ "url": url }, 
+    {
+        "$set": {
+             "downloaded": True, 
+             "file": os.path.basename(mp4_fullpath),
+             "fullpath": mp4_fullpath
+        }
+    }, upsert = False).matched_count
+
+def change_fullpath(file, fullpath):
+    return videoCollection.update_one({ "file": file }, 
+    {
+        "$set": {
+             "fullpath": fullpath
+        }
+    }, upsert = False).matched_count
+
+def set_downloaded_flag(url, flag):
+    return videoCollection.update_one({ "url": url }, 
+    {
+        "$set": { "downloaded": flag}
+    }, upsert = False).matched_count
+
+def set_scenes(videoFileName, scenes):
+    return videoCollection.update_one({ "file": videoFileName }, 
+    {
+        "$set": { "scenes": scenes}
+    }, upsert = False).matched_count
+
+
+def get_shows():
+    return list(videoCollection.distinct("show"))
+
+def get_show_seasons(targetShow):
+    return list(videoCollection.distinct("season", {"show": targetShow})) 
+
+def delete_by_file(videoFileName):
+    return videoCollection.delete_one({"file": videoFileName}).deleted_count
+
+def delete_by_url(url):
+    return videoCollection.delete_one({"url": url}).deleted_count
+
+def delete_show(targetShow):
+    return videoCollection.delete_many({"show": targetShow}).deleted_count
