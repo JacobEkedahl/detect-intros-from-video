8b6e9b1c23d8b00d6b7e5d443fcce6f5ec1be7ec
Tiago Redaelli <tiago.redaelli@gmail.com>
2019-12-27 15:31:55 +0100

    time handler updates

--- src/utils/time_handler.py
+++ src/utils/time_handler.py
@@ -1,6 +1,5 @@
-import re
 import time
-
+import re 
 
 def timestamp(time_string):
     if ',' in time_string:
@@ -20,14 +19,37 @@ def timestamp(time_string):
         milliseconds = 0
     return (hours*3600 + minutes*60 + seconds) * 1000 + milliseconds
 
+
+def validate_timeformat(t):
+    try: 
+        if not (t[0].isdigit() and t[1].isdigit() and t[2] == ':' and t[3].isdigit() and t[4].isdigit() and t[5] == ':' and t[6].isdigit() and t[7].isdigit()):
+            return False 
+        if len(t) == 8:
+            return True
+        if ',' in t:
+            t = t.split(',')[1]
+            for c in t: 
+                if not c.isdigit():
+                    return False
+        elif '.' in t: 
+            t = t.split('.')[1]
+            for c in t: 
+                if not c.isdigit():
+                    return False
+        return True 
+    except: 
+        return False
+
+
 def timestamp_to_str(timestamp):
     milliseconds = timestamp%1000
     milliseconds = int(milliseconds)
     seconds = (timestamp/1000)%60
     seconds = int(seconds)
-    minutes= (timestamp/(1000*60))%60
+    minutes = (timestamp/(1000*60))%60
     minutes = int(minutes)
     hours= (timestamp/(1000*60*60))%24
     if milliseconds > 0:
         return "%02d:%02d:%02d.%03d" % (hours, minutes, seconds, milliseconds)
     return "%02d:%02d:%02d" % (hours, minutes, seconds)
+
